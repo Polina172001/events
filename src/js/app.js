d9ebb2be61cf35b843dc8468bf107addf12e58c6
Polina Filippova <polina17012001@mail.ru>
2021-09-10 14:32:26 +0300

    version1

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/extensions */
+import addField from './field';
+import addImage from './image';
+
+addField(4);
+setInterval(() => addImage(4), 2000);
+
+// TODO: write code here
